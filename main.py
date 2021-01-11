2acbc5c952eb6984d455f0d191bf1341d7e55f85
aleksandrax <zarkovska.a@inbox.lv>
2021-01-11 12:49:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#komentarus
+#var rakstīk
+#vairakas rindas"""
+print("Sveika, pasaule!")
+print(2 * 2)
+print(2 * 2, 2 * 3, 2 * 4, "Pitons")
+print(f"Ja saskaitīsim 5 ar 7 iegūsim {5+7}.") #Kombinētā izdrūka
+print("Sveika, "+"pasaule!")
+print("Aleksandra "*5)
+print("2021 gada būs",365*86400,"sekundes")
+print(0.1+0.2-0.3)
+print(1/2)
+print()
